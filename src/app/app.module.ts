46906e99f38e60db3adf418413c47f6ade399b17
MennaAbdallah <menna.eid.95@gmail.com>
2020-08-12 00:57:46 +0200

    add contact-us empty component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { WhyChooseUsComponent } from './components/why-choose-us/why-choose-us.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactUsComponent,
+    IntroComponent,
+    AboutUsComponent,
+    WhyChooseUsComponent,
+    PortfolioComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
