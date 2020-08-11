b1f37afb3c699c16bff30d4c9bbca9d2bb23e6d1
MennaAbdallah <menna.eid.95@gmail.com>
2020-08-12 01:00:56 +0200

    add navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  currentLang: string;
+
+  constructor(public translate: TranslateService) {
+    this.currentLang = localStorage.getItem('currentLang') || 'en';
+    this.translate.use(this.currentLang);
+  }
+
+  changeCurrentLang(lang: string) {
+    this.translate.use(lang);
+    localStorage.setItem('currentLang', lang);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
