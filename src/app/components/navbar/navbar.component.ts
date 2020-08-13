ca2ee4534609c38605e48d6b742cfeff4fe08986
MennaAbdallah <menna.eid.95@gmail.com>
2020-08-13 16:29:59 +0200

    make navbar right align while arabic translation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,15 +9,34 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   currentLang: string;
+  currentDirection: string;
+  textAlign: string;
 
   constructor(public translate: TranslateService) {
     this.currentLang = localStorage.getItem('currentLang') || 'en';
+    this.currentDirection = localStorage.getItem('currentDirection') || 'ltr';
+    this.textAlign = localStorage.getItem('textAlign') || 'left';
+
     this.translate.use(this.currentLang);
+    document.body.setAttribute('style', 'direction: ' + this.currentDirection + ';text-align: ' + this.textAlign);
   }
 
   changeCurrentLang(lang: string) {
-    this.translate.use(lang);
+
+    if (lang === 'en') {
+      this.currentDirection = 'ltr';
+      this.textAlign = 'left';
+    } else if (lang === 'ar') {
+      this.currentDirection = 'rtl';
+      this.textAlign = 'right';
+    }
+
     localStorage.setItem('currentLang', lang);
+    localStorage.setItem('currentDirection', this.currentDirection);
+    localStorage.setItem('textAlign', this.textAlign);
+
+    this.translate.use(lang);
+    document.body.setAttribute('style', 'direction: ' + this.currentDirection + ';text-align: ' + this.textAlign);
   }
 
   ngOnInit(): void {
